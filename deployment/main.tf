6d3c64b23d7df31884350863b40f348b42b23bc2
eddoog <codewithalfredo@gmail.com>
2024-06-08 18:32:38 +0700

    feat: setup iam policy through terraform

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,16 +1,31 @@
 provider "google" {
-  project = "capstone-425808"
-  region  = "asia-southeast2"
+  project = var.gcp_project_id
+  region  = var.cloudrun_location
 }
 
-resource "google_cloud_run_v2_service" "production" {
-  name     = "service-a"
 
-  location = "asia-southeast1"
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = var.cloudrun_location
+  project  = var.gcp_project_id
+  service  = var.cloudrun_name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+resource "google_storage_bucket_iam_policy" "editor" {
+  bucket = google_storage_bucket.bucket.name
+  policy_data = data.google_iam_policy.viewer.policy_data
+}
+
+
+resource "google_cloud_run_v2_service" "production" {
+  name     = var.cloudrun_name
+
+  location = var.cloudrun_location
 
   template {
     containers {
-      image = "asia-southeast2-docker.pkg.dev/capstone-425808/capstone/app:latest"
+      image = var.image
       volume_mounts {
         name       = "bucket"
         mount_path = "/models"
@@ -28,6 +43,26 @@ resource "google_cloud_run_v2_service" "production" {
 }
 
 resource "google_storage_bucket" "bucket" {
-    name     = "cloudrun-service"
-    location = "asia-southeast2"
+    project = var.gcp_project_id
+    name     = var.bucket_name
+    location = var.bucket_location
+    storage_class = "STANDARD"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+data "google_iam_policy" "viewer" {
+  binding {
+    role = "roles/storage.objectViewer"
+    members = [
+        "allUsers",
+    ] 
+  }
 }
---
+++ deployment/variables.tf
@@ -0,0 +1,54 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# You must define the following environment variables.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# GOOGLE_CREDENTIALS
+# or
+# GOOGLE_APPLICATION_CREDENTIALS
+
+variable "gcp_project_id" {
+  description = "The ID of the GCP project in which these resources will be created."
+  default     = "capstone-425808"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "cloudrun_location" {
+  description = "The Zone to launch the Cloud Instance into."
+  type        = string
+  default     = "asia-southeast2"
+}
+
+variable "cloudrun_name" {
+    description = "The Name of the Cloud Run service to create."
+    type        = string
+    default     = "capstone"
+    
+}
+
+variable "bucket_name" {
+  description = "The Name of the example Bucket to create."
+  type        = string
+  default     = "bucket-capstone-425808"
+}
+
+variable "bucket_location" {
+  description = "The location to store the Bucket. This value can be regional or multi-regional."
+  type        = string
+  default     = "asia-southeast2"
+}
+
+variable "image" {
+  description = "The Docker image to deploy to Cloud Run."
+  type        = string
+  default     = "asia-southeast2-docker.pkg.dev/capstone-425808/capstone/app:latest"
+}
