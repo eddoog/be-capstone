14e9a301d8a03faf39befc3c3026f3b3112aeb96
eddoog <codewithalfredo@gmail.com>
2024-06-08 17:45:04 +0700

    feat: modify tf

--- deployment/main.tf
+++ deployment/main.tf
@@ -1,28 +1,33 @@
-resource "google_cloud_run_v2_service" "default" {
-  name     = "cloudrun-service"
+provider "google" {
+  project = "capstone-425808"
+  region  = "asia-southeast2"
+}
 
-  location     = "asia-southeast2"
+resource "google_cloud_run_v2_service" "production" {
+  name     = "service-a"
+
+  location = "asia-southeast1"
 
   template {
     containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      image = "asia-southeast2-docker.pkg.dev/capstone-425808/capstone/app:latest"
       volume_mounts {
         name       = "bucket"
-        mount_path = "/var/www"
+        mount_path = "/models"
       }
     }
 
     volumes {
       name = "bucket"
       gcs {
-        bucket    = google_storage_bucket.default.name
+        bucket    = google_storage_bucket.bucket.name
         read_only = false
       }
     }
   }
 }
 
-resource "google_storage_bucket" "default" {
+resource "google_storage_bucket" "bucket" {
     name     = "cloudrun-service"
-    location = "US"
+    location = "asia-southeast2"
 }
