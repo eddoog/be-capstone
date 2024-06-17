6ac219a271ae2774859ba63fc7c1cfd22fcbc2a5
eddoog <codewithalfredo@gmail.com>
2024-06-18 02:07:16 +0700

    fix: fix ordering result

--- pkg/predict.go
+++ pkg/predict.go
@@ -1,16 +1,12 @@
 package pkg
 
 import (
-	"fmt"
-
 	tf "github.com/galeone/tensorflow/tensorflow/go"
 	tg "github.com/galeone/tfgo"
 )
 
 func PredictWithModel(stationMapTf map[string]*tf.Tensor) {
 	model := tg.LoadModel("model/halim", []string{"serve"}, nil)
-
-	fmt.Println(*model)
 
 	result := model.Exec([]tf.Output{
 		model.Op("StatefulPartitionedCall", 0),
@@ -19,9 +15,6 @@ func PredictWithModel(stationMapTf map[string]*tf.Tensor) {
 		model.Op("serving_default_input_2", 0): stationMapTf["Halim"],
 	})
 
-	floodResult := result[0].Value().([][]float32)
-	climateResult := result[1].Value().([][]float32)
-
-	fmt.Println("Flood_Branch result:", floodResult)
-	fmt.Println("Climate_Branch result:", climateResult)
+	climateResult := result[0].Value().([][]float32)
+	floodResult := result[1].Value().([][]float32)
 }
